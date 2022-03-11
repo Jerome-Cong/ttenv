9e794cc67ebace8e031d1974bb69545915ad60d9
Shijie Cong <s.cong@student.tudelft.nl>
2022-03-11 14:20:30 +0100

    modified for pyyaml6.0

--- ttenv/maps/map_utils.py
+++ ttenv/maps/map_utils.py
@@ -21,7 +21,7 @@ def round(x):
 
 class GridMap(object):
     def __init__(self, map_path, margin2wall=0.5):
-        map_config = yaml.load(open(map_path+".yaml", "r"))
+        map_config = yaml.safe_load(open(map_path+".yaml", "r"))
         self.map = np.loadtxt(map_path+".cfg")
         if 'empty' in map_path:
             self.map = None
